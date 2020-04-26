722e129cfa6552f1bf66eb3283594e5d3b434b21
Far2k <elferno@mail.ru>
2020-04-26 17:26:18 +0000

    Task 6.1 done

--- 06_control_structures/task_6_1.py
+++ 06_control_structures/task_6_1.py
@@ -12,4 +12,7 @@
 '''
 
 mac = ['aabb:cc80:7000', 'aabb:dd80:7340', 'aabb:ee80:7000', 'aabb:ff80:7000']
-
+mac_cisco= []
+for result in mac:
+    mac_cisco.append(result.replace(':','.'))
+print(mac_cisco)
